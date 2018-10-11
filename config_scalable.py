866aa5f2efcf69a2a681f20f72d507cd1b508e14
Isa Dutse <ijdutse@yahoo.com>
2018-10-11 22:44:49 +0100

    second commit!

--- config_scalable.py
+++ config_scalable.py
@@ -2,119 +2,75 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from collections import defaultdict, Counter
-from datetime import datetime
-import preprocessor as p
-import random, os, utils, smart_open, json, codecs, pickle, time
-import gensim
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument, LabeledSentence
-from sklearn.feature_extraction.text import HashingVectorizer, TfidfVectorizer
+import matplotlib.ticker
+import itertools
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from scipy.fftpack import fft
-
-
-# Example of list of Raw Data: data_sources = ['diverse_tweets1.json']
-
-#def main():
- #   spd = Spd(data_sources) #class instantiation
-  #  start = time.clock()
-   # relevant_tweets = spd.detector(data_sources)
-    #stop = time.clock()
-    #return relevant_tweets
 
 
 
-
-class Semantics:
+class Scale:
     """Class constructor may be activated later, for now it is muted!
     def __init__(self, dataframe): # defines the constructor:
          self.dataframe = dataframe
      """
     pass
 
-    def get_tweets_stream(self, dataframe): # iteratively yield the tweets in a given file
+    def stream(self,dataframe): # iteratively yield the tweets in a given file
+        self.dataframe=dataframe
         with open(dataframe, 'r') as csv:
             next(csv) # this skips the header in the file
             for line in csv:
                 cdate, tweet = line[2:21], line[22:-1]
                 yield cdate, tweet
-    def get_tweet_batch(self, tweets_stream, batch_size=50): #return tweets in batches of n tweets, default is 50
-        self.tweets_stream = tweets_stream
-        self.batch_size = batch_size
-        tweets = []
-        try:
-            for _ in range(batch_size):
-                _, tweet = next(tweets_stream)
-                tweets.append(tweet)
-        except StopIteration:
-            return None, None
-        return tweets
+    def batch_sim(self,tweets_stream, batch_size=10): #1 compute similarity between pair of tweets iteratively
+            self.tweets_stream = tweets_stream
+            self.batch_size = batch_size
+            all_other_tweets = []
+            tweets = []
+            similarity_scores = []
+            try:
+                for _ in range(batch_size):
+                    cdate, tweet = next(tweets_stream)
+                    tweets.append(tweet)
+            except StopIteration:
+                return None, None
+            for i in range(2):
+                try:
+                    for anchor_tweet, other_tweet in itertools.zip_longest(tweets[1:], tweets[2:]): #2 zip tweets
+                        tweet_pair = [] #3 return pair of tweets
+                        anchor_tweet = tweets[1]
+                        all_other_tweets.append(other_tweet)
+                        tweet_pair.append(anchor_tweet),tweet_pair.append(other_tweet)
+                        vectorizer = TfidfVectorizer()
+                        vectorised_tweets = vectorizer.fit_transform(tweet_pair) #4 tfidf matrix of tweets pair
+                        # compute cosine similarity between the tweet pair:
+                        cosim = np.round(cosine_similarity(vectorised_tweets.toarray()[0].reshape(1,-1),\
+                                                           vectorised_tweets.toarray()[1].reshape(1,-1)).flatten(),2)
+                        if cosim[0] == 0.0 or cosim[0]== 1.0:
+                            continue
+                        similarity_scores.append(cosim[0])#yield cosim, #tweet_pair
+                        del tweet_pair
+                except:
+                    continue
+            return similarity_scores, anchor_tweet, all_other_tweets#tweet_pair
+if __name__=='__main__':
+    p = Scale()
 
-    def get_tfidf_matrix(self, tweets): # convert tweets from get_tweet_batch() to numeric using tfidf scheme ...
-        self.tweets = tweets  # or call the member function returning list of tweets
-        vectorizer = TfidfVectorizer()
-        vectorised_tweets = vectorizer.fit_transform(tweets)
-        return vectorised_tweets.toarray()
-
-    # pairwise similarity using the usual approach and approach proposed for the study
-    def pairwise_sim1(self, vectorised_tweets): # compute pairwise similarity using usual cosine similarity
-        self.vectorised_tweets = vectorised_tweets
-        tfidf_matrix = vectorised_tweets
-        sim_scores = []
-        for row in tfidf_matrix:
-            cosim = np.round(cosine_similarity(row.reshape(1,-1), tfidf_matrix).flatten(),2)
-            sim_scores.append(cosim)
-        return sim_scores
-
-    def pairwise_sim2(self, vectorised_tweets): # compute pairwise similarity using modified row-wise
-        self.vectorised_tweets = vectorised_tweets
-        tfidf_matrix = vectorised_tweets
-        sim_scores = []
-        y_axis = []
-        j = tfidf_matrix.shape[0]
-        while j > 1:
-            i = 0
-            c = 1
-            for index in range(i, j):
-                if index == 0:
-                    continue # avoid multiplying a row by itself
-                cosim = np.round(cosine_similarity(tfidf_matrix[i:c].reshape(1,-1),\
-                                tfidf_matrix[index:]).flatten() ,2)
-                #y_axis.append(cosim)
-                sim_scores.append(cosim)
-                # update indices ...
-                j -=1
-                i +=1
-                c +=1
-                # plot the results, if necessary
-        for score in sim_scores: # return scores as single array/list
-            y_axis.extend(score)
-        return y_axis
-    # get most similar or top scores ... a relevent function:
-    def top_scores2(self, sim_scores): # using the argpartition function of numpy for the operation
-        self.sim_scores = sim_scores
-        sim_score_index = []
-        for array in sim_scores:
-            index = np.argpartition(array, -array.argmax())[-9:] # partition on highest score to return last 9
-            sim_score = array[index]
-            sim_score_index.append((sim_score, index))
-            # visualisation using the score_index to plot the values:
-        return sim_score_index
-    def top_scores1(self, sim_scores): # more accurate procedure  based on elimination trick ...
-        self.sim_scores = sim_scores
-        sim_scores_and_indices = []
-        for array in sim_scores:
-            #scores_and_indices = []
-            while array.shape[0] > 1:
-                sim_scores_and_indices.append((array.max(), array.argmax()))
-                array = np.delete(array, array.argmax())
-        # visualisation using the scores_and_indices ...
-        return sim_scores_and_indices
-    def ff_transform(self, sim_scores): #Transformation of sim_scores in time domain to frequency domain ..
-        self.sim_scores = sim_scores
-        time_scores = np.array(sim_scores)
-        frequency_scores = fft(time_scores)
-        # plot the result:
-        return frequency_scores
-#if __name__ =='__main__':
- #   main()
+    tweet_stream = p.stream('test.csv')
+    sim_scores, anchor_tweet, other_tweets = p.batch_sim(tweet_stream, batch_size=51)
+    print(sim_scores)
+    print(anchor_tweet)
+    print(other_tweets)
+    print(len(sim_scores))
+    print(len(other_tweets))
+    plt.plot(sim_scores)
+    plt.plot(range(0,len(sim_scores)),sim_scores)
+    locator = matplotlib.ticker.MultipleLocator(4) # format the x-axis to print integers:
+    plt.gca().xaxis.set_major_locator(locator)
+    #formatter =  matplotlib.ticker.StrMethodFormatter("{x:0f}") # format sring if required
+    #plt.gca().xaxis.set_major_formatter(formatter)
+    plt.xlabel('Index')
+    plt.ylabel('Scores')
+    plt.title('Similarity between anchor and other tweets')
+    plt.show()
