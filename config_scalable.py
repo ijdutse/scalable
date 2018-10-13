7ff67205d0c4cc871256f44a91978d0e7bc1a0be
Isa Dutse <ijdutse@yahoo.com>
2018-10-13 22:37:09 +0100

    The 3rd commit operation!

--- config_scalable.py
+++ config_scalable.py
@@ -4,73 +4,82 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker
 import itertools
+from itertools import zip_longest as zipper
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
 
-class Scale:
-    """Class constructor may be activated later, for now it is muted!
-    def __init__(self, dataframe): # defines the constructor:
-         self.dataframe = dataframe
-     """
-    pass
+class Scalable (object):
+    """Python class to compute similarity between n tweets in a finite window that can be scaled to accommodate tweets efficiently. Initialise/trigger
+    the class to create instances by supplying path to the file containing the data and the window size of batch size to return"""
 
-    def stream(self,dataframe): # iteratively yield the tweets in a given file
-        self.dataframe=dataframe
-        with open(dataframe, 'r') as csv:
+    def __init__(self, dataframe, window_size): # defines the constructor:
+         self.dataframe = dataframe
+         self.window_size = window_size
+
+    def tweets_stream(self):
+        """ THis function iteratively yield tweets from a csv file containing tweets"""
+        with open(self.dataframe, 'r') as csv:
             next(csv) # this skips the header in the file
             for line in csv:
                 cdate, tweet = line[2:21], line[22:-1]
                 yield cdate, tweet
-    def batch_sim(self,tweets_stream, batch_size=10): #1 compute similarity between pair of tweets iteratively
-            self.tweets_stream = tweets_stream
-            self.batch_size = batch_size
-            all_other_tweets = []
-            tweets = []
-            similarity_scores = []
-            try:
-                for _ in range(batch_size):
-                    cdate, tweet = next(tweets_stream)
-                    tweets.append(tweet)
-            except StopIteration:
-                return None, None
-            for i in range(2):
-                try:
-                    for anchor_tweet, other_tweet in itertools.zip_longest(tweets[1:], tweets[2:]): #2 zip tweets
-                        tweet_pair = [] #3 return pair of tweets
-                        anchor_tweet = tweets[1]
-                        all_other_tweets.append(other_tweet)
-                        tweet_pair.append(anchor_tweet),tweet_pair.append(other_tweet)
-                        vectorizer = TfidfVectorizer()
-                        vectorised_tweets = vectorizer.fit_transform(tweet_pair) #4 tfidf matrix of tweets pair
-                        # compute cosine similarity between the tweet pair:
-                        cosim = np.round(cosine_similarity(vectorised_tweets.toarray()[0].reshape(1,-1),\
-                                                           vectorised_tweets.toarray()[1].reshape(1,-1)).flatten(),2)
-                        if cosim[0] == 0.0 or cosim[0]== 1.0:
-                            continue
-                        similarity_scores.append(cosim[0])#yield cosim, #tweet_pair
-                        del tweet_pair
-                except:
-                    continue
-            return similarity_scores, anchor_tweet, all_other_tweets#tweet_pair
-if __name__=='__main__':
-    p = Scale()
 
-    tweet_stream = p.stream('test.csv')
-    sim_scores, anchor_tweet, other_tweets = p.batch_sim(tweet_stream, batch_size=51)
+    def tweets_batch(self,stream, window_size = 50):
+        """This function utilises the tweets_stream generator function to return m number of tweets as list"""
+        self.stream = stream
+        self.window_size = window_size
+        tweets = [] # stores batch of tweets
+        try:
+            for _ in range(self.window_size):
+                cdate, tweet = next(stream)
+                tweets.append(tweet)
+        except StopIteration:
+            return None, None
+        return tweets
+
+    def tweets_sim(self, tweets_list):
+        """The following function computes the pairwise similarity between paair of random tweets using cosine similarity"""
+        self.tweets_list = tweets_list
+        similarity_scores = [] # #keeps track of similarity scores > 0.0 and < 1.0
+        discarded = 0 # keeps tract of pairs with simialrity of 0.0 or 1.0
+        for anchor_tweet, other_tweet in zipper(tweets_list[1:], tweets_list[2:]):
+            tweet_pair = [] #  only stores the pair of tweets for simialrity computation. This is being created and deleted continously
+            if other_tweet in tweets_list[2:][-1]: # avoids TypeError on reaching the final tweet in the list
+                break
+            anchor_tweet = tweets_list[1] # make the anchor tweet constant for each iteration in the window
+            tweet_pair.append(anchor_tweet), tweet_pair.append(other_tweet)# stores pairs for simialrity computation only
+            vectorizer = TfidfVectorizer(lowercase = False)
+            vectorised_pair = vectorizer.fit_transform(tweet_pair) # convert tweet_pair to numeric using tfidf scheme
+            cosim = np.round(cosine_similarity(vectorised_pair.toarray()[0].reshape(1,-1), vectorised_pair.toarray()[1].reshape(1,-1)).flatten(),2)
+            if cosim[0] == 0.0 or cosim[0] == 1.0:
+                discarded +=1
+                continue
+            similarity_scores.append(cosim[0])
+        return similarity_scores
+
+
+    def view_results(self, scores):
+        self.scores = scores
+        """This function visualises the pairwise simialrity scores between pair of tweets"""
+        plt.plot(range(len(scores)),scores)
+        # format the x-axis to print integers at certain intervals:
+        locator = matplotlib.ticker.MultipleLocator(10)
+        plt.gca().xaxis.set_major_locator(locator)
+        #formatter =  matplotlib.ticker.StrMethodFormatter("{x:0f}")
+        #plt.gca().xaxis.set_major_formatter(formatter)
+        plt.xlabel('Index')#, rotation=90)
+        plt.ylabel('Scores')
+        plt.title('Similarity between anchor and other tweets')
+        plt.xticks(rotation=90)
+        plt.show()
+
+if __name__=='__main__':
+    """The main function to initialise/trigger the Scalable class to make its methods available"""
+    trigger = Scalable('test.csv',100)
+    stream = trigger.tweets_stream() # stream of tweets to pull out m batches
+    batch = trigger.tweets_batch(stream, 313) # batch of tweets from stream of tweets
+    sim_scores = trigger.tweets_sim(batch) #
     print(sim_scores)
-    print(anchor_tweet)
-    print(other_tweets)
-    print(len(sim_scores))
-    print(len(other_tweets))
-    plt.plot(sim_scores)
-    plt.plot(range(0,len(sim_scores)),sim_scores)
-    locator = matplotlib.ticker.MultipleLocator(4) # format the x-axis to print integers:
-    plt.gca().xaxis.set_major_locator(locator)
-    #formatter =  matplotlib.ticker.StrMethodFormatter("{x:0f}") # format sring if required
-    #plt.gca().xaxis.set_major_formatter(formatter)
-    plt.xlabel('Index')
-    plt.ylabel('Scores')
-    plt.title('Similarity between anchor and other tweets')
-    plt.show()
+    view_result = trigger.view_results(sim_scores) # visualise simialriy scores
