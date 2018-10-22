e5e6ad24a582a3ecb20101241f6c7bb436c07588
Isa Dutse <ijdutse@yahoo.com>
2018-10-22 15:06:44 +0100

    The eight commit operation - time included

--- config_static.py
+++ config_static.py
@@ -23,105 +23,21 @@ class Static(object):
         with open(self.dataframe, 'r') as csv:
             next(csv) # this skips the header in the file
             for line in csv:
-                cdate, tweet = line[2:21], line[22:-1]
-                yield cdate, tweet
+                time, tweet = line[0:5], line[5:-1] # tuple of posting times converted to seconds and tweets
+                yield time, tweet
 
     def tweets_batch(self,stream, window_size = 50):
-        """This function utilises the tweets_stream generator function to return m number of tweets as list"""
+        """This function utilises the tweets_stream generator function to return m number of tuples of posting times and tweets as list"""
         self.stream = stream
         self.window_size = window_size
-        tweets = [] # stores batch of tweets
+        time_tweet = [] # stores posting time and tweets in batches
         try:
             for _ in range(self.window_size):
-                cdate, tweet = next(stream)
-                tweets.append(tweet)
+                time, tweet = next(stream)
+                time_tweet.append((time, tweet))
         except StopIteration:
             return None, None
-        return tweets
-
-    def tweets_sim(self, tweets_list):
-        """The following function computes the pairwise similarity between paair of random tweets using cosine similarity"""
-        self.tweets_list = tweets_list
-        similarity_scores = [] # #keeps track of similarity scores > 0.0 and < 1.0
-        discarded = 0 # keeps tract of pairs with simialrity of 0.0 or 1.0
-        for anchor_tweet, other_tweet in zipper(tweets_list[1:], tweets_list[2:]):
-            tweet_pair = [] #  only stores the pair of tweets for simialrity computation. This is being created and deleted continously
-            if other_tweet in tweets_list[2:][-1]: # avoids TypeError on reaching the final tweet in the list
-                break
-            anchor_tweet = tweets_list[1] # make the anchor tweet constant for each iteration in the window
-            tweet_pair.append(anchor_tweet), tweet_pair.append(other_tweet)# stores pairs for simialrity computation only
-            vectorizer = TfidfVectorizer(lowercase = False)
-            vectorised_pair = vectorizer.fit_transform(tweet_pair) # convert tweet_pair to numeric using tfidf scheme
-            cosim = np.round(cosine_similarity(vectorised_pair.toarray()[0].reshape(1,-1), vectorised_pair.toarray()[1].reshape(1,-1)).flatten(),2)
-            if cosim[0] == 0.0 or cosim[0] == 1.0:
-                discarded +=1
-                continue
-            similarity_scores.append(cosim[0])
-        return similarity_scores
-    def tweets_sim1(self, tweets_list):
-        # THIS FUNCTION IS SIMILAR TO THE window function below:
-        """The following function computes the pairwise similarity between paair of random tweets using cosine similarityself.
-        This function dffers from the the above (tweet_sim) as it considers all the possible anchors in the given window. for
-        instance, a window of size 11 will have 10 possible anchors, hence 10 different windows of disproportionate sizes"""
-        self.tweets_list = tweets_list
-        anchor_index = 0
-        other_index = 1
-        all_similarity_scores = []
-        tracker = []
-        while other_index < len(tweets_list)-1:
-            #anchor = anchor
-            similarity_scores = [] # #keeps track of similarity scores > 0.0 and < 1.0
-            discarded = 0 # keeps tract of pairs with simialrity of 0.0 or 1.0
-            for anchor_tweet, other_tweet in zipper(tweets_list[anchor_index:], tweets_list[other_index:]):
-                tweet_pair = [] #  only stores the pair of tweets for simialrity computation. This is being created and deleted continously
-                if other_tweet in tweets_list[1:][-1]: # avoids TypeError on reaching the final tweet in the list
-                    break
-                anchor_tweet = tweets_list[anchor_index] # make the anchor tweet constant for each iteration in the window
-                tweet_pair.append(anchor_tweet), tweet_pair.append(other_tweet)# stores pairs for simialrity computation only
-                vectorizer = TfidfVectorizer(lowercase = False)
-                vectorised_pair = vectorizer.fit_transform(tweet_pair) # convert tweet_pair to numeric using tfidf scheme
-                cosim = np.round(cosine_similarity(vectorised_pair.toarray()[0].reshape(1,-1), vectorised_pair.toarray()[1].reshape(1,-1)).flatten(),2)
-                if cosim[0] == 0.0 or cosim[0] == 1.0:
-                    discarded +=1
-                    continue
-                similarity_scores.append(cosim[0])
-                all_similarity_scores.append(cosim[0])
-            tracker.append((anchor_index, len(similarity_scores))) # keep tracks of the anchor index and the number of similar tweets to it in the window
-            anchor_index +=1
-            other_index +=1
-        return all_similarity_scores, tracker
-
-    def window (self, tweets_list):
-        """This function takes list of tweets and return single window and relevant metrics - list of Top Scores, dictionary
-         of tracker consisting of Top scores for each anchor tweet and the corresponding number of the top scores"""
-        self.tweets_list = tweets_list
-        frame = {} # initialise empty frame to store all window instances and associated values
-        for r in range(5):
-            frame['Window_'+str(r)] = {'Top Scores':[],'Tracker':[]} # for each window store its top scores, tuple of top scores and indices
-            anchor_index = 0
-            other_index = 1
-            while other_index < len(tweets_list)-1:
-                discarded_pair = 0 # keeps track of number of pairs with simialrity of 0.0 or 1.0
-                retained_pair = 0 # keeps track of number of pairs greater than 0.0 and less than 1.0
-                for anchor_tweet, other_tweet in zipper(tweets_list[anchor_index:], tweets_list[other_index:]):
-                    tweet_pair = [] #  only stores the pair of tweets for simialrity computation. This is being created and deleted continously
-                    if other_tweet in tweets_list[1:][-1]: # avoids TypeError on reaching the final tweet in the list
-                        break
-                    anchor_tweet = tweets_list[anchor_index] # make the anchor tweet constant for each iteration in the window
-                    tweet_pair.append(anchor_tweet), tweet_pair.append(other_tweet)# stores pairs for simialrity computation only
-                    vectorizer = TfidfVectorizer(lowercase = False)
-                    vectorised_pair = vectorizer.fit_transform(tweet_pair) # convert tweet_pair to numeric using tfidf scheme
-                    cosim = np.round(cosine_similarity(vectorised_pair.toarray()[0].reshape(1,-1), vectorised_pair.toarray()[1].reshape(1,-1)).flatten(),2)
-                    if cosim[0] == 0.0 or cosim[0] == 1.0:
-                        discarded_pair +=1
-                        continue
-                    frame['Window_'+str(r)]['Top Scores'].append(cosim[0]) # updtae the frame data structure with the anchor tweet and
-                    retained_pair +=1
-                frame['Window_'+str(r)]['Tracker'].append((anchor_index, retained_pair)) # updtae the frame data structure with the anchor tweet and
-                anchor_index +=1 # pick the next tweet as the next anchor
-                other_index +=1 # shrink the window size by a factor of 1
-
-        return window
+        return time_tweet
 
     def frame(self, tweets_list, frame_size = 3):
         """This function takes list of tweets and return set of windows. A frame is a unit of computation that can take very
@@ -147,34 +63,37 @@ class Static(object):
                         if other_tweet in window_tweet[1:][-1]: # avoids TypeError on reaching the final tweet in the list
                             break
                         anchor_tweet = window_tweet[anchor_index] # make the anchor tweet constant for each iteration in the window
-                        tweet_pair.append(anchor_tweet), tweet_pair.append(other_tweet)# stores pairs for simialrity computation only
+                        tweet_pair.append(anchor_tweet[1]), tweet_pair.append(other_tweet[1])# stores pairs for simialrity computation only
                         vectorizer = TfidfVectorizer(lowercase = False)
                         vectorised_pair = vectorizer.fit_transform(tweet_pair) # convert tweet_pair to numeric using tfidf scheme
                         cosim = np.round(cosine_similarity(vectorised_pair.toarray()[0].reshape(1,-1), vectorised_pair.toarray()[1].reshape(1,-1)).flatten(),2)
                         if cosim[0] == 0.0 or cosim[0] == 1.0:
                             discarded_pair +=1
                         elif cosim[0] > 0.5:
-                            frame['Window_'+str(window)]['Top Scores'].append(cosim[0]) # update top scores wrt to the anchor
+                            frame['Window_'+str(window)]['Top Scores'].append((anchor_tweet[0],cosim[0])) # update top scores wrt to the anchor
                         else:
-                            frame['Window_'+str(window)]['All Scores'].append(cosim[0]) # updtae the frame data structure with the anchor tweet and
+                            frame['Window_'+str(window)]['All Scores'].append((anchor_index, anchor_tweet[0],cosim[0])) # updtae the frame data structure with the anchor tweet and
                             retained_pair +=1
 
                     frame['Window_'+str(window)]['Counter'].append((anchor_index, retained_pair)) # updtae the frame data structure with the anchor tweet and
                     # update the score Tracker
-                    q = np.array(frame['Window_'+str(window)]['All Scores']) # convert list of top scores to np array for computational ease
+                    """q = np.array(frame['Window_'+str(window)]['All Scores']) # convert list of scores in pos. in the tuple to np array for computational ease
                     while q.shape[0]>1: # execute as long as length of the array is > 1
                         frame['Window_'+str(window)]['Score Tracker'].append((q.max(),q.argmax())) # track top scores and their indices in windows
-                        q = np.delete(q, q.argmax()) #  delete used score and index
+                        q = np.delete(q, q.argmax())""" #  delete used score and index
                     # update indices of anchor and other tweets
                     anchor_index +=1 # pick the next tweet as the next anchor
                     other_index +=1 # shrink the window size by a factor of 1
             # update stopping criteria:
             k+=1
         return frame
+
+
 # VISUALISATIONS:
-    def view_results(self, frame):
+    def plot_line(self, frame):
         self.frame = frame
-        """This function visualises the pairwise simialrity scores for a number of windows in a frame"""
+        """This function visualises the pairwise simialrity scores for a number of windows in a frame
+        using a line plot"""
         for key in frame:
             scores = [] # store top scores for every window in the frame
             for score in frame[key]['All Scores']:
@@ -209,17 +128,53 @@ class Static(object):
             plt.xticks(rotation=90)
             plt.show()
 
+    def plot_scatter(self, frame):
+        self.frame = frame
+        for window in frame:
+            time_in_sec = [] # stores posting times
+            sim_scores = [] # stores similarity of pairs during  posting time
+            visited_items = set() # avoid duplicate values due to nested loops
+            const = 3 # useful in scaling the size parameter in the scatter plot (optional)
+            for item in frame[window]['All Scores']:
+                if item in visited_items:
+                    continue
+                # check for possible occurence of ',' in time and convert to float at index 1 before appending
+                if (item[1][0] == ',') and (item[1][-1]==','):
+                    time_in_sec.append(float(item[1][1:-1]))
+                elif (item[1][0]==','):
+                    time_in_sec.append(float(item[1][1:]))
+                elif (item[1][-1]==','):
+                    time_in_sec.append(float(item[1][:-1]))
+                else:
+                    time_in_sec.append(float(item[1]))
+                sim_scores.append(item[2]) # stores similarity score of pairs at index 2 in the tuple
+                visited_items.add(item) # add item to set of visited items
+            # visualise output as line plot and scatter plot:
+            #plt.plot(time_in_sec, sim_scores) # all posting times and all scores
+            #sizes = [sim_scores.count(score)**2*const for score in sim_scores] #make multiple scores standout(optional)
+            sizes = [sim_scores.count(score) for score in sim_scores]
+            #plt.scatter(time_in_sec, sim_scores, s=sizes) # scatter plot with standout scores
+            plt.scatter(time_in_sec, sim_scores, s = sizes)
+            plt.xlabel('Posting Time (s)')
+            plt.ylabel('Scores')
+            plt.title('Similarity between anchor and other tweets in '+window)
+            plt.xticks(rotation=90)
+            plt.show()
+
+
 # MAIN ... run all:
 if __name__=='__main__':
     """The main function to initialise/trigger the Scalable class to make its methods available"""
     #trigger = Scalable('test.csv',100) # the window_size here is being overshadowed by the window_size in tweets_batch function
-    trigger = Static('test.csv',101) # recent name of the class to reflect the the windowing type i.e. Static
+    #trigger = Static('test_X.csv',101) # recent name of the class to reflect the the windowing type i.e. Static
+    trigger = Static('test_X1.csv',30) # normalisedtime set as index
     stream = trigger.tweets_stream() # stream of tweets to pull out m batches
-    batch = trigger.tweets_batch(stream, window_size=90)#, 313) # batch of tweets from stream of tweets
+    batch = trigger.tweets_batch(stream, window_size=160)#, 313) # batch of tweets from stream of tweets
     #sim_scores = trigger.tweets_sim(batch) # print(sim_scores)
     #window = trigger.frame(batch) # single window and many anchor tweets
-    frame = trigger.frame(batch, frame_size=3) #  many windows and multiple anchor tweets for each window
+    frame = trigger.frame(batch, frame_size=4) #  many windows and multiple anchor tweets for each window
     #view_result0 = trigger.view_results(sim_scores) # visualise simialriy scores
-    print(frame)
-    view_result = trigger.view_results(frame) # visualise simialriy scores
-    bar_chart = trigger.plot_bar(frame) # visualise simialriy scores
+    #print(frame)
+    #line_plot = trigger.plot_line(frame) # visualise simialriy scores in form of line plot
+    #bar_chart = trigger.plot_bar(frame) # visualise simialriy scores in form of bar chart
+    scatter_plot = trigger.plot_scatter(frame) # visualise outcome as scatter plot
